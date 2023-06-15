5acfc4c08c91efc332ca7199e4c552c7f9b40995
H7CDR <110644782+H7CDR@users.noreply.github.com>
2023-06-15 15:50:13 +1000

    Added Camera and brightness

---
+++ Assets/Script/LigtChange.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LigtChange : MonoBehaviour
+{
+    public float luxDefault, luxBright;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
